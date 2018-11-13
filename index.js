63f4a3c957da294997516143d165fe8f69879121
Lucy Wall <lhwall@gmail.com>
2018-11-13 20:54:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,8 @@ function destructivelyUpdateDriverWithKeyAndValue(driver,key, value){
   driver[key] = value
   return driver
 }
+
+function deleteFromDriverByKey(driver,key){
+  newObject = Object.assign({}, driver)
+  delete newObject.key
+}
