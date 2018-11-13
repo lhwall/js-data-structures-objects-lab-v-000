2442f2205102e99958f0b53eea4474f526642223
Lucy Wall <lhwall@gmail.com>
2018-11-13 20:51:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,9 +3,10 @@
 const driver = {}
 
 function updateDriverWithKeyAndValue(driver,key,value){
-
+  driver.assign({}, driver, key: value)
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver,key, value){
   driver[key] = value
+  return driver
 }
